2189f4b4f571be7f1b2e41a4475ae49ed5e31a1e
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2023-04-11 19:49:30 -0300

    Function Date

--- Helpers.php
+++ Helpers.php
@@ -1,18 +1,23 @@
 <?php
 echo '<br>Arquivos de Funções</br>';
-function saudacao(){
-   $hora =5;
-   $saudacao1  = 'se lasque';
-   if ($hora>=0 && $hora<=5){
-      return $saudacao1; 
-   }
-   if ($hora<=11){
-      $saudacao1  = 'Boa tarde!';
+function saudacao()
+{
+   $hora = date('H', 'I','S');
+   echo $hora;
+   echo '<br></br>';
+   $saudacao1  = 'Bom dia';
+   if ($hora >= 0 and $hora <= 5) {
       return $saudacao1;
-    
-   }
-}
-function resumirTexto($texto,int  $limite, $continue = '...'){
-   return $texto;  
+   } else if ($hora >= 6 and $hora <= 12) {
+      $saudacao1 = 'Boa tarde!';
+      return $saudacao1;
+   } else {
+      $saudacao1 = 'Adeus';
+      return $saudacao1;
+       }
 }
 
+function resumirTexto($texto, int  $limite, $continue = '...')
+{
+   return $texto;
+}
--- Index.php
+++ Index.php
@@ -4,5 +4,5 @@ include_once 'Helpers.php';
 echo '<h1>Arquivo Index</h1>';
 
 echo saudacao();
-echo'<hr>';
-echo resumirTexto('Meu nome é Miriam', 50,'...' );
+// echo'<hr>';
+// echo resumirTexto('Meu nome é Miriam', 50,'...' );
