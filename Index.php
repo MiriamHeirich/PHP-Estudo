b52c90694ebc3b402ee4743c6464da8c237332fb
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2023-04-12 20:27:41 -0300

    Operador Ternário

--- Index.php
+++ Index.php
@@ -9,3 +9,15 @@ $texto= 'Contagem de caracteres';
 echo $texto = mb_strlen(trim($texto));
 // echo'<hr>';
 // echo resumirTexto('Meu nome é Miriam', 50,'...' );
+
+echo '<br></br>';
+$valor = 5;
+// if ($valor){
+//    echo $valor;
+// }
+// else{
+//    echo  0;
+// }
+
+//Operador Ternário da função IF acima.
+echo ($valor ? $valor:0);
