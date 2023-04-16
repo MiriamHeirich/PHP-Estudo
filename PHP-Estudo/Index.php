295dced61c1c39b87868b88cd2e620572a664605
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2023-04-16 16:39:58 -0300

    Constantes

---
+++ PHP-Estudo/Index.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'config.php';
+include_once 'Helpers.php';
+
+// echo '<h1>Arquivo Index</h1>';
+
+// echo saudacao();
+// echo'<hr>';
+
+// echo'<hr>';
+// $valor = 5;
+//     echo'<hr>';
+//     echo contarTempo('2023-04-13 00:05:15');
+
+//  if (validarEmail('teste')){
+//      echo 'Email válido';
+//  }else {
+//     echo'Email inválido';
+//  }
+ $url='teste';
+ var_dump(validarUrl($url));
+echo '<hr>';
+ var_dump(validarUrlcomFiltro($url));
---
+++ PHP-Estudo/config.php
@@ -0,0 +1,8 @@
+<?php
+echo '<br>Configuração do sistema</br>';
+date_default_timezone_set('America/Sao_Paulo');
+
+define ('SITE_NOME', 'UnSet');
+define ('SITE_DESCRICAO', 'UnSet- Tecnologia em Sistemas');
+define ('URL_PRODUCAO', 'http://unset.com.br');
+define ('URL_PRODUCAO', 'http://localhost/P')
