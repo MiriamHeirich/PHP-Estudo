24b448c5e5c5f0e208b7e06efa9d0ea908720247
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2023-04-19 19:50:25 -0300

    CSS

--- Index.php
+++ Index.php
@@ -1,18 +1,41 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
-<title>Estudo em PHP</title>
-<meta charset="utf-8">
+	<title>Estudo em PHP</title>
+	<meta charset="utf-8">
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f5f5f5;
+		}
+		h1 {
+			color: #333;
+			text-align: center;
+			margin-top: 50px;
+		}
+		.container {
+			max-width: 800px;
+			margin: 0 auto;
+			padding: 20px;
+			background-color: #fff;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		}
+	</style>
 </head>
 <body>
-
-<?php
+	<div class="container">
+		<?php
 require_once 'config.php';
 include_once 'Helpers.php';
  
 echo ''.saudacao().' <br> '.'Hoje é'.'  '. dataAtual();
 echo'<br>';
-
 ?>
+	</div>
 </body>
+</html
+
+
+
+
 
