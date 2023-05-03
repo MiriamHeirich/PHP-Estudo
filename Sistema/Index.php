aed7c81c3b87a9548ab62250c59c2f38da684bdf
Miriam Mehret <101216878+MiriamHeirich@users.noreply.github.com>
2023-05-03 12:09:33 -0300

    Apache

--- Sistema/Index.php
+++ Sistema/Index.php
@@ -1,16 +1,16 @@
 
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5..0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<?php
 	
-	require_once 'config.php';
-	include_once 'C:\xampp\htdocs\blog\Sistema\Nucleo\Helpers.php';
-	include 'C:\xampp\htdocs\blog\Sistema\Nucleo\Mensagem.php';
-	use Sistema\Nucleo\Mensagem;
-		 $msg = new Mensagem;
-		 echo $msg->sucesso('Sucesso mensagem')->renderizar();
-		 echo '<hr>';
+	// require_once 'config.php';
+	// include_once 'C:\xampp\htdocs\blog\Sistema\Nucleo\Helpers.php';
+	// include 'C:\xampp\htdocs\blog\Sistema\Nucleo\Mensagem.php';
+	// use Sistema\Nucleo\Mensagem;
+	// 	 $msg = new Mensagem;
+	// 	 echo $msg->sucesso('Sucesso mensagem')->renderizar();
+	// 	 echo '<hr>';
+	require 'C:\xampp\htdocs\blog\vendor\autoload.php';
+	echo saudacao();
 		
-?>
 
 
 
--- Sistema/Nucleo/Helpers.php
+++ Sistema/Nucleo/Helpers.php
@@ -2,135 +2,121 @@
 
 class Helpers{
   
- //Tudo que for diferente de 9 e 0 será substituido por vazio.
- //   echo "CPF:". $limparNumero = preg_replace('/[^0-9]/','',$CPF);
-echo '<br>Arquivos de Funções</br>';
-  echo '<hr>';
-//   $CPF='058.068.129-79';
-//   var_dump(validarCPF($CPF));
-//  function validarCPF(string $CPF):bool{
-//    if(mb_strlen($CPF)!= 11 or preg_match('/(\d)\1{10}/',$CPF) ){
+   
+// function dataAtual() {
+//    $diaMes = date('d');
+//    $diaSemana = date('w');
+//    $mes= date('n')-1;
+//    $ano = date('Y');
+//    $nomeDiasdaSemana= ['segunda','terça','quarta','quinta','sexta','sabado','domingo'];
+//    $nomeDosMeses = ['janeiro','fevereiro','março','abril','maio','junho'
+//                       ,'julho','agosto','setembro','outubro','novembro','dezembro'];
+
+//       $dataFormatada = $nomeDiasdaSemana[$diaSemana].','.$diaMes.' de ' .$nomeDosMeses[$mes].' de '.$ano;
+//       return $dataFormatada;
+// }
+
+
+
+// function url(string $url) {
+//    $servidor = filter_input(INPUT_SERVER, 'SERVER_NAME');
+//    $ambiente = ($servidor == 'localhost'? URL_DESENVOLVIMENTO : URL_PRODUCAO);
+
+// if(str_starts_with($url,'/'))
+// {
+//    return $ambiente.$url;
+// }
+//    return $ambiente.'/'.$url;
+// }
+
+
+// /**
+//  * localhost
+//  * Define o localhost
+//  * 
+//  * @param string $servidor recebe o nome do servidor
+//  * @return boolean retorna o servidor
+//  */
+
+// function localhost():bool{
+//    $servidor = filter_input(INPUT_SERVER, 'SERVER_NAME');
+//    if ($servidor == 'localhost'){
+//       return true;
+//    }
+//       return false;
+// }
+
+// function validarEmail(string $email): bool{
+//    return filter_var ($email, FILTER_VALIDATE_EMAIL);//filter em PHP possui várias validações
+// }
+
+// /**
+//  * validarUrl
+//  * Verifica se uma Url é válida
+//  * 
+//  * @param string $url recebe a url digitada
+//  * @return boolean retorna se a url éválida
+//  */
+
+// function validarUrl(string $url): bool{
+   
+//    if(mb_strlen($url)<10){
 //       return false;
 //    }
-//    return true;}
- 
-
-
-
-function dataAtual() {
-   $diaMes = date('d');
-   $diaSemana = date('w');
-   $mes= date('n')-1;
-   $ano = date('Y');
-   $nomeDiasdaSemana= ['segunda','terça','quarta','quinta','sexta','sabado','domingo'];
-   $nomeDosMeses = ['janeiro','fevereiro','março','abril','maio','junho'
-                      ,'julho','agosto','setembro','outubro','novembro','dezembro'];
-
-      $dataFormatada = $nomeDiasdaSemana[$diaSemana].','.$diaMes.' de ' .$nomeDosMeses[$mes].' de '.$ano;
-      return $dataFormatada;
-}
-
-
-
-function url(string $url) {
-   $servidor = filter_input(INPUT_SERVER, 'SERVER_NAME');
-   $ambiente = ($servidor == 'localhost'? URL_DESENVOLVIMENTO : URL_PRODUCAO);
-
-if(str_starts_with($url,'/'))
-{
-   return $ambiente.$url;
-}
-   return $ambiente.'/'.$url;
-}
-
-
-/**
- * localhost
- * Define o localhost
- * 
- * @param string $servidor recebe o nome do servidor
- * @return boolean retorna o servidor
- */
-
-function localhost():bool{
-   $servidor = filter_input(INPUT_SERVER, 'SERVER_NAME');
-   if ($servidor == 'localhost'){
-      return true;
-   }
-      return false;
-}
-
-function validarEmail(string $email): bool{
-   return filter_var ($email, FILTER_VALIDATE_EMAIL);//filter em PHP possui várias validações
-}
-
-/**
- * validarUrl
- * Verifica se uma Url é válida
- * 
- * @param string $url recebe a url digitada
- * @return boolean retorna se a url éválida
- */
-
-function validarUrl(string $url): bool{
    
-   if(mb_strlen($url)<10){
-      return false;
-   }
-   
-   if (!str_contains($url, '.')){
-      return false;
-   }
-   if (str_contains($url, 'http://') or str_contains($url, 'https://')){
-      return true;
-   }
+//    if (!str_contains($url, '.')){
+//       return false;
+//    }
+//    if (str_contains($url, 'http://') or str_contains($url, 'https://')){
+//       return true;
+//    }
 
-      return false;
+//       return false;
 
-}
+// }
 
 
-function validarUrlcomFiltro (string $url): bool{
-   return filter_var ($url, FILTER_VALIDATE_URL);
-}
+// function validarUrlcomFiltro (string $url): bool{
+//    return filter_var ($url, FILTER_VALIDATE_URL);
+// }
 
 
-/**
- * contarTempo
- * Pega a hora atual e reduz da hora em que foi postado, segundo  banco de dados
- * 
- *  @param string $data pega hora no banco de dados
- *  @param string $agora pega hora atual
- *  @param string $diferenca subtrai hora da postagem da hora atual
- *  @param string $segundo calcula quantos segundos se passaram
- *  @param string $minutos calcula quantos minutos se passaram
- *  @param string $horas calcula quantas horas se passaram
- *  @param string $dias calcula quantos dias se passaram
- *  @param string $semanas calcula quantas semanas se passaram
- *  @param string $meses calcula quantos meses se passaram
- *  @param string $anos calcula quantos anos se passaram
- *  @return string retorna a data e hora da postagem
- */
-function contarTempo (string $data){
-   echo $agora = strtotime(date('Y-m-d  H:i:s'));
-   echo'<hr>';
-   echo $tempo = strtotime($data);
-   echo'<hr>';
-   echo $diferenca = ($agora - $tempo);
-   $segundo = $diferenca;
-   $horas = round($diferenca/3600);
-   $minutos= round($diferenca/60);
-   $dias = round($diferenca/86400);
-   $semanas = round($diferenca/604800);
-   $meses = round($diferenca/2419200);
-   $anos = round($diferenca / 29030400);
-   echo'<hr>';
-   if ($segundo <= 60){
-      return  'agora';
-   }
-   elseif($minutos <=60){
-      return ($minutos == 1 ? 'Há um minuto': 'Há '.$minutos.'minutos');
-;   }
+// /**
+//  * contarTempo
+//  * Pega a hora atual e reduz da hora em que foi postado, segundo  banco de dados
+//  * 
+//  *  @param string $data pega hora no banco de dados
+//  *  @param string $agora pega hora atual
+//  *  @param string $diferenca subtrai hora da postagem da hora atual
+//  *  @param string $segundo calcula quantos segundos se passaram
+//  *  @param string $minutos calcula quantos minutos se passaram
+//  *  @param string $horas calcula quantas horas se passaram
+//  *  @param string $dias calcula quantos dias se passaram
+//  *  @param string $semanas calcula quantas semanas se passaram
+//  *  @param string $meses calcula quantos meses se passaram
+//  *  @param string $anos calcula quantos anos se passaram
+//  *  @return string retorna a data e hora da postagem
+//  */
+// function contarTempo (string $data){
+//    echo $agora = strtotime(date('Y-m-d  H:i:s'));
+//    echo'<hr>';
+//    echo $tempo = strtotime($data);
+//    echo'<hr>';
+//    echo $diferenca = ($agora - $tempo);
+//    $segundo = $diferenca;
+//    $horas = round($diferenca/3600);
+//    $minutos= round($diferenca/60);
+//    $dias = round($diferenca/86400);
+//    $semanas = round($diferenca/604800);
+//    $meses = round($diferenca/2419200);
+//    $anos = round($diferenca / 29030400);
+//    echo'<hr>';
+//    if ($segundo <= 60){
+//       return  'agora';
+//    }
+//    elseif($minutos <=60){
+//       return ($minutos == 1 ? 'Há um minuto': 'Há '.$minutos.'minutos');
+// ;   }
 
 }
 
@@ -180,18 +166,17 @@ function saudacao()
 }
 
 
-/**
- * resumirTexto
- * Resume um texto...........................................
- * 
- * @param string $texto texto para resumir
- * @param int  $limite quantidade de caracteres
- * @param string $continue exibe ao final do resumo 
- * @return string texto resumido
- */
-function resumirTexto($texto, int  $limite, $continue = '...')
-{
-   return $texto;
-}
+// /**
+//  * resumirTexto
+//  * Resume um texto...........................................
+//  * 
+//  * @param string $texto texto para resumir
+//  * @param int  $limite quantidade de caracteres
+//  * @param string $continue exibe ao final do resumo 
+//  * @return string texto resumido
+//  */
+// function resumirTexto($texto, int  $limite, $continue = '...')
+// {
+//    return $texto;
+// }
 
-}
